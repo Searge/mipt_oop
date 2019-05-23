2e61edb974c0e7dd4b1d3dc889f18566ef1a8d92
Searge <ashikcygan@gmail.com>
2019-05-23 15:24:17 +0300

    Base Classes | Refactor 2

--- week_2/base_classes.py
+++ week_2/base_classes.py
@@ -39,19 +39,7 @@ class A(Base):
                     for (x, y) in zip(self.data, self.result)])
 
 
-class B:
-
-    def __init__(self, data, result):
-        self.data = data
-        self.result = result
-
-    def get_answer(self):
-        return [int(x >= 0.5) for x in self.data]
-
-    def get_score(self):
-        ans = self.get_answer()
-        return sum([int(x == y)
-                    for (x, y) in zip(ans, self.result)]) / len(ans)
+class B(Base):
 
     def get_loss(self):
         return -sum([y * math.log(x) + (1 - y) * math.log(1 - x)
@@ -75,19 +63,7 @@ class B:
         return 2 * pre * rec / (pre + rec)
 
 
-class C:
-
-    def __init__(self, data, result):
-        self.data = data
-        self.result = result
-
-    def get_answer(self):
-        return [int(x >= 0.5) for x in self.data]
-
-    def get_score(self):
-        ans = self.get_answer()
-        return sum([int(x == y)
-                    for (x, y) in zip(ans, self.result)]) / len(ans)
+class C(Base):
 
     def get_loss(self):
         return sum([abs(x - y)
