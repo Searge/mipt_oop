9ce694ca8569c3a1b56e21f8d0a2c09fefc859c3
Searge <ashikcygan@gmail.com>
2019-05-29 18:06:27 +0300

    Decorator Exercise

---
+++ week_3/exercises/deco.py
@@ -0,0 +1,29 @@
+class Hero:
+    def __init__(self):
+        self.positive_effects = []
+        self.negative_effects = []
+
+        self.stats = {
+            "HP": 128,
+            "MP": 42,
+            "SP": 100,
+
+            "Strength": 15,
+            "Perception": 4,
+            "Endurance": 8,
+            "Charisma": 2,
+            "Intelligence": 3,
+            "Agility": 8,
+            "Luck": 1
+        }
+
+    def get_positive_effects(self):
+        return self.positive_effects.copy()
+
+    def get_negative_effects(self):
+        return self.negative_effects.copy()
+
+    def get_stats(self):
+        return self.stats.copy()
+
+    # ...
