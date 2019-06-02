6580afeeb57f639db6100f93ba4fb69dc46f6759
Searge <ashikcygan@gmail.com>
2019-06-02 15:07:21 +0300

    Adapter exercise: :zap:

--- week_3/exercises/adaptee.py
+++ week_3/exercises/adaptee.py
@@ -51,13 +51,28 @@ class System:
         self.lightmap = light_mapper.lighten(self.map)
 
 
-class MappingAdapter:
+class MappingAdapter(Light):
     def __init__(self, adaptee):
         self.adaptee = adaptee
+
+    def _get_lights_and_obstacles(self, grid):
+        lights = []
+        obstacles = []
+        for i, row in enumerate(grid):
+            for j, elem in enumerate(row):
+                if elem == 1:
+                    lights += [(j, i)]
+                if elem == -1:
+                    obstacles += [(j, i)]
+        return lights, obstacles
 
     def lighten(self, grid):
         dim = (len(grid[0]), len(grid))
         self.adaptee.set_dim(dim)
+        lights, obstacles = self._get_lights_and_obstacles(grid)
+        self.adaptee.set_lights(lights)
+        self.adaptee.set_obstacles(obstacles)
+        return self.adaptee.generate_lights()
 
 
 if __name__ == '__main__':
