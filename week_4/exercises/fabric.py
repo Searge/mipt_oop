16604633f287d941a3aba0a85e8eb8ed06360d2d
Searge <ashikcygan@gmail.com>
2019-06-05 07:18:36 +0300

    Abstract Factory exercise: :tada:

---
+++ week_4/exercises/fabric.py
@@ -0,0 +1,56 @@
+import random
+
+
+class AbstractLevel:
+    @classmethod
+    def get_map(cls):
+        pass
+
+    @classmethod
+    def get_objects(cls):
+        pass
+
+
+class EasyLevel(AbstractLevel):
+    pass
+
+
+class EasyMap:
+
+    def __init__(self):
+        self._map = [[0 for j in range(5)] for i in range(5)]
+        for i in range(5):
+            for j in range(5):
+                if i == 0 or j == 0 or i == 4 or j == 4:
+                    # граница карты
+                    self._map[j][i] = -1
+                else:
+                    # случайная характеристика области
+                    self._map[j][i] = random.randint(0, 2)
+
+    def get_map(self):
+        return self._map
+
+
+class EasyObjects:
+
+    def __init__(self):
+        # размещаем переход на след. уровень
+        self.objects = [('next_lvl', (2, 2))]
+
+    def get_objects(self, map_obj):
+        # размещаем противников
+        for obj_name in ['rat']:
+            coord = (random.randint(1, 3), random.randint(1, 3))
+            # ищем случайную свободную локацию
+            intersect = True
+            while intersect:
+                intersect = False
+                for obj in self.objects:
+                    if coord == obj[1]:
+                        intersect = True
+                        coord = (random.randint(1, 3), random.randint(1, 3))
+
+            self.objects.append((obj_name, coord))
+
+        return self.objects
