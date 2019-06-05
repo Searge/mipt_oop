68fd0ac1ac0a866986d5c433858246020cacc8f7
Searge <ashikcygan@gmail.com>
2019-06-05 15:46:55 +0300

    Abstract Factory exercise: :sparkles:

--- week_4/exercises/fabric.py
+++ week_4/exercises/fabric.py
@@ -2,19 +2,12 @@ import random
 
 
 class AbstractLevel:
-    class Level:
-        def __init__(self, name):
-            self.name = name
 
     class Map:
         pass
 
     class Object:
         pass
-
-    @classmethod
-    def create_lvl(cls, name):
-        return cls.Level(name)
 
     @classmethod
     def get_map(cls):
@@ -57,19 +50,94 @@ class EasyLevel(AbstractLevel):
                     for obj in self.objects:
                         if coord == obj[1]:
                             intersect = True
-                            coord = (random.randint(1, 3), random.randint(1, 3))
+                            coord = (random.randint(1, 3),
+                                     random.randint(1, 3))
 
                 self.objects.append((obj_name, coord))
 
             return self.objects
 
 
-def create_lvl(factory):
-    lvl = factory.create_lvl(factory)
-    map_ = factory.get_map()
-    obj = factory.get_objects()
+class MediumLevel(AbstractLevel):
+    class Map:
+        def __init__(self):
+            self.Map = [[0 for j in range(8)] for i in range(8)]
+            for i in range(8):
+                for j in range(8):
+                    if i == 0 or j == 0 or i == 7 or j == 7:
+                        # граница карты
+                        self.Map[j][i] = -1
+                    else:
+                        # случайная характеристика области
+                        self.Map[j][i] = random.randint(0, 2)
+
+        def get_map(self):
+            return self.Map
+
+    class Objects:
+        def __init__(self):
+            self.objects = [('next_lvl', (4, 4))]
+
+        def get_objects(self, map):
+            for obj_name in ['rat', 'snake']:
+                coord = (random.randint(1, 6), random.randint(1, 6))
+                intersect = True
+                while intersect:
+                    intersect = False
+                    for obj in self.objects:
+                        if coord == obj[1]:
+                            intersect = True
+                            coord = (random.randint(1, 6),
+                                     random.randint(1, 6))
+
+                self.objects.append((obj_name, coord))
+
+            return self.objects
+
+
+class HardLevel(AbstractLevel):
+    class Map:
+        def __init__(self):
+            self.Map = [[0 for j in range(10)] for i in range(10)]
+            for i in range(10):
+                for j in range(10):
+                    if i == 0 or j == 0 or i == 9 or j == 9:
+                        # граница карты :: непроходимый участок карты
+                        self.Map[j][i] = -1
+                    else:
+                        # случайная характеристика области
+                        self.Map[j][i] = random.randint(-1, 8)
+
+        def get_map(self):
+            return self.Map
+
+    class Objects:
+        def __init__(self):
+            self.objects = [('next_lvl', (5, 5))]
+
+        def get_objects(self, map):
+            for obj_name in ['rat', 'snake']:
+                coord = (random.randint(1, 8), random.randint(1, 8))
+                intersect = True
+                while intersect:
+                    intersect = False
+                    if map[coord[0]][coord[1]] == -1:
+                        intersect = True
+                        coord = (random.randint(1, 8), random.randint(1, 8))
+                        continue
+                    for obj in self.objects:
+                        if coord == obj[1]:
+                            intersect = True
+                            coord = (random.randint(1, 8),
+                                     random.randint(1, 8))
+
+                self.objects.append((obj_name, coord))
+            return self.objects
 
 
 if __name__ == "__main__":
     factory = EasyLevel()
-    lvl = create_lvl(factory)
+    map_ = factory.get_map()
+    obj = factory.get_objects()
+    print(map_, obj, sep='\n')
+    print(map_._map)
